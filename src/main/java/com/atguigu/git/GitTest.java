4936f947ca4bf83d6cc7fd8faf05677df9853dca
qzh <qzh@gmail.com>
2022-05-29 18:52:27 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2022-05-29 18:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
